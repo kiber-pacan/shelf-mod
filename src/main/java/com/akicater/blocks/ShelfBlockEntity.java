9de11a0c86a2bdaf7039827debb0fd7b5c0c1361
kiber-pacan <markpustog@gmail.com>
2024-08-14 21:46:18 +0500

    dis too

---
+++ src/main/java/com/akicater/blocks/ShelfBlockEntity.java
@@ -0,0 +1,82 @@
+
+package com.akicater.blocks;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.block.BlockEntityProvider;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.inventory.Inventories;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.network.listener.ClientPlayPacketListener;
+import net.minecraft.network.packet.Packet;
+import net.minecraft.network.packet.s2c.play.BlockEntityUpdateS2CPacket;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.collection.DefaultedList;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.akicater.Shelfmod.SHELF_BLOCK_ENTITY;
+
+public class ShelfBlockEntity extends BlockEntity implements BlockEntityProvider {
+    public final DefaultedList<ItemStack> inventory = DefaultedList.ofSize(4, ItemStack.EMPTY);
+
+    public ShelfBlockEntity(BlockPos pos, BlockState state) {
+        super(SHELF_BLOCK_ENTITY, pos, state);
+    }
+
+    @Nullable
+    @Override
+    public Packet<ClientPlayPacketListener> toUpdatePacket() {
+        return BlockEntityUpdateS2CPacket.create(this);
+    }
+
+    @Override
+    public NbtCompound toInitialChunkDataNbt() {
+        return createNbt();
+    }
+
+    @Override
+    public void readNbt(NbtCompound nbt) {
+        super.readNbt(nbt);
+        inventory.clear();
+        Inventories.readNbt(nbt, inventory);
+        markDirty();
+    }
+
+    @Override
+    public void writeNbt(NbtCompound nbt) {
+        super.writeNbt(nbt);
+        Inventories.writeNbt(nbt, inventory);
+    }
+
+    @Override
+    public void markDirty() {
+        if (this.world != null) {
+            markDirtyInWorld(this.world, this.pos, this.getCachedState());
+        }
+    }
+
+    protected void markDirtyInWorld(World world, BlockPos pos, BlockState state) {
+        world.markDirty(pos);
+
+        if (!world.isClient()) {
+            ((ServerWorld) world).getChunkManager().markForUpdate(pos);
+        }
+    }
+
+    public void clear() {
+        inventory.clear();
+    }
+
+    @Override
+    public @Nullable BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
+        return new ShelfBlockEntity(pos, state);
+    }
+}
